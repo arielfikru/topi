8f47527e5421f0bda12aa7615dbdcf5de46ad367
arielfikru <71917936+arielfikru@users.noreply.github.com>
2023-11-11 08:28:19 +0700

    Add this

--- torrentp/torrent_info.py
+++ torrentp/torrent_info.py
@@ -1,21 +1,27 @@
+import libtorrent as lt
+
 class TorrentInfo:
     def __init__(self, path, libtorrent):
         self._path = path
         self._lt = libtorrent
         self._info = self._lt.torrent_info(self._path)
 
-    def show_info(self):
-        pass
+    def list_files(self):
+        """ List files in the torrent """
+        file_list = []
+        for file_index, file in enumerate(self._info.files()):
+            file_list.append({"id": file_index, "file": file.path})
+        return file_list
 
     def create_torrent_info(self):
         self._info = self._lt.torrent_info(self._path)
         return self._info
 
     def __str__(self):
-        pass
+        return f"TorrentInfo(path={self._path})"
 
     def __repr__(self):
-        pass
+        return f"TorrentInfo(path={self._path})"
 
     def __call__(self):
         return self.create_torrent_info()
