06492051c004c9fd6af4b7c4b8a2dd4485dc7ce9
arielfikru <71917936+arielfikru@users.noreply.github.com>
2023-11-11 08:46:07 +0700

    FIX MAGNET

--- torrentp/torrent_info.py
+++ torrentp/torrent_info.py
@@ -1,10 +1,10 @@
 import libtorrent as lt
+import time
 
 class TorrentInfo:
     def __init__(self, path_or_magnet, libtorrent, session):
         self._lt = libtorrent
         self._session = session
-        self._info = None
         if path_or_magnet.startswith("magnet:"):
             self._info = self._download_metadata(path_or_magnet)
         else:
@@ -13,7 +13,7 @@ class TorrentInfo:
     def _download_metadata(self, magnet_uri):
         magnet_handle = self._lt.add_magnet_uri(self._session, magnet_uri, {'save_path': '.'})
         while not magnet_handle.has_metadata():
-            time.sleep(1)
+            time.sleep(1)  # Wait until metadata is available
         return magnet_handle.get_torrent_info()
 
     def list_files(self):
@@ -24,14 +24,13 @@ class TorrentInfo:
         return file_list
 
     def create_torrent_info(self):
-        self._info = self._lt.torrent_info(self._path)
         return self._info
 
     def __str__(self):
-        return f"TorrentInfo(path={self._path})"
+        return f"TorrentInfo(path_or_magnet={self._path_or_magnet})"
 
     def __repr__(self):
-        return f"TorrentInfo(path={self._path})"
+        return f"TorrentInfo(path_or_magnet={self._path_or_magnet})"
 
     def __call__(self):
         return self.create_torrent_info()
