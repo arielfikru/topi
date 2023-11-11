ab2c8498b1440b309b29329d3167e153cb1f261b
arielfikru <71917936+arielfikru@users.noreply.github.com>
2023-11-11 08:45:40 +0700

    FIX MAGNET

--- torrentp/torrent_downloader.py
+++ torrentp/torrent_downloader.py
@@ -4,37 +4,31 @@ from .downloader import Downloader
 import libtorrent as lt
 
 class TorrentDownloader:
-    def __init__(self, path_or_magnet, save_path):
-        self._file_path = path_or_magnet
+    def __init__(self, file_path, save_path):
+        self._file_path = file_path
         self._save_path = save_path
-        self._downloader = None
-        self._torrent_info = None
         self._lt = lt
-        self._file = None
-        self._add_torrent_params = None
-        self._session = Session(self._lt)
+        self._session = Session(self._lt).create_session()  # Inisialisasi sesi libtorrent
 
     def start_download(self, download_speed=0, upload_speed=0):
         if self._file_path.startswith('magnet:'):
             self._add_torrent_params = self._lt.parse_magnet_uri(self._file_path)
             self._add_torrent_params.save_path = self._save_path
-            self._downloader = Downloader(session=self._session(), torrent_info=self._add_torrent_params,
+            self._downloader = Downloader(session=self._session, torrent_info=self._add_torrent_params,
                                           save_path=self._save_path, libtorrent=lt, is_magnet=True)
-
         else:
-            self._torrent_info = TorrentInfo(self._file_path, self._lt)
-            self._downloader = Downloader(session=self._session(), torrent_info=self._torrent_info(),
-                                          save_path=self._save_path, libtorrent=None, is_magnet=False)
+            self._torrent_info = TorrentInfo(self._file_path, self._lt, self._session)
+            self._downloader = Downloader(session=self._session, torrent_info=self._torrent_info,
+                                          save_path=self._save_path, libtorrent=lt, is_magnet=False)
 
         self._session.set_download_limit(download_speed)
         self._session.set_upload_limit(upload_speed)
-
         self._file = self._downloader
         self._file.download()
 
     def list_torrent_files(self):
         """ List files in the torrent """
-        torrent_info = TorrentInfo(self._file_path, self._lt)
+        torrent_info = TorrentInfo(self._file_path, self._lt, self._session)
         return torrent_info.list_files()
 
     def __str__(self):
